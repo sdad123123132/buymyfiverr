9175adc3abdb806643e97492aac64502ac2ef148
Tiffany Mayumi <entity493@gmail.com>
2018-12-21 08:17:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ bot.on('message', msg => {
 
 bot.on('message', msg => {
   if (msg.content === '!check.bot.connection') {
-    msg.channel.SendMessage("If you see this message im connected");
+    msg.reply("If you see this message im connected");
   }
 });
 
